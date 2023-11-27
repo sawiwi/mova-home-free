0601dc33cb6e4336ddc8cec619f4d430bd622b82
fabian <fabian.salas.astete@gmail.com>
2023-11-27 12:41:46 -0300

    feat: add new footer

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,9 +1,10 @@
 import { FaFacebookF,FaInstagram } from "react-icons/fa6";
-import { FaYoutube } from "react-icons/fa";
+import { FaYoutube, FaLinkedin  } from "react-icons/fa";
 import {AiOutlineWhatsApp} from 'react-icons/ai';
 export const iconList = {
     FaFacebookF,
     FaInstagram ,
     FaYoutube ,
+    FaLinkedin ,
     AiOutlineWhatsApp
 };
